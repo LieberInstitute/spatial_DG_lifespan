d33d58731e3bd2c087ac92606f726df199e1b179
lcolladotor <lcollado@jhsph.edu>
2022-04-08 14:44:51 -0400

    Abby: Ignore the rds file
    Leo: Tested the code with spatialLIBD v1.7.15. Aka https://github.com/LieberInstitute/spatialLIBD/commit/eb0ae5acd5dd0bb3f2dd4bb03290ba541153d145 worked
    
    Co-authored-by: Heena Divecha <heenadivecha@gmail.com>
    Co-authored-by: Abby Spangler <aspangle@gmail.com>
    
    Former-commit-id: fef0ecd3cf91f68c1db95a581e6e6bf23303ca47 [formerly 25b69a55a6fd7ede1fcd1886aa56d7e1e2511ed2]
    Former-commit-id: 27f70c67835463614bdd81be603eda61d3223d1a

--- code/02_build_spe/01_build_spe.R
+++ code/02_build_spe/01_build_spe.R
@@ -54,8 +54,7 @@ spe <- read10xVisiumWrapper(
     type = "sparse",
     data = "raw",
     images = c("lowres", "hires", "detected", "aligned"),
-    load = TRUE,
-    reference_gtf = "/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-gex-GRCh38-2020-A/genes/genes.gtf"
+    load = TRUE
 )
 Sys.time()
 
