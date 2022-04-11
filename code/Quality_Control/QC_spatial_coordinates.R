411f4ca5cb54a7676cd861898b6c14dc628d55b8
Anthony Ramnauth <aramnau1@jh.edu>
2022-04-11 13:56:55 -0400

    Adding script of ggspavis package to check spatial coordinates and orientation are accurate.

---
+++ code/Quality_Control/QC_spatial_coordinates.R
@@ -0,0 +1,46 @@
+###################################################################################################
+# spatial_DG_lifespan project
+# Check spatial coordinates & orientation of individual capture areas within spe object are correct
+# Anthony Ramnauth, Apr 11 2022
+###################################################################################################
+
+setwd("/dcs04/lieber/marmaypag/lifespanDG_LIBD001/spatial_DG_lifespan/")
+
+library(SpatialExperiment)
+library(ggspavis)
+library(sessioninfo)
+library(here)
+
+# Create directory for QC plots
+dir_rdata <- here::here("plots", "QC_plots")
+dir.create(dir_rdata, showWarnings = FALSE, recursive = TRUE)
+
+# Load spe
+spe <- readRDS(here::here("processed-data", "02_build_spe","spe.rds"))
+
+# Plot spatial coordinates of within tissue spots
+pdf(file = here::here("plots", "QC_plots", "spe_Spatial_Coordinates.pdf"))
+plotSpots(spe = spe[, colData(spe)$sample_id == "Br8686"],
+    x_coord = "pxl_col_in_fullres",
+    y_coord = "pxl_row_in_fullres",
+    size = 1,
+    in_tissue = "in_tissue")
+plotSpots(spe = spe[, colData(spe)$sample_id == "Br2706"],
+    x_coord = "pxl_col_in_fullres",
+    y_coord = "pxl_row_in_fullres",
+    size = 1,
+    in_tissue = "in_tissue")
+plotSpots(spe = spe[, colData(spe)$sample_id == "Br3942"],
+    x_coord = "pxl_col_in_fullres",
+    y_coord = "pxl_row_in_fullres",
+    size = 1,
+    in_tissue = "in_tissue")
+plotSpots(spe = spe[, colData(spe)$sample_id == "Br6023"],
+    x_coord = "pxl_col_in_fullres",
+    y_coord = "pxl_row_in_fullres",
+    size = 1,
+    in_tissue = "in_tissue")
+
+dev.off()
+
+sessionInfo()
