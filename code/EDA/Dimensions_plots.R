07fb573027761101919d5fbdb1fba9d2f22d4478
Anthony Ramnauth <aramnau1@jh.edu>
2022-04-26 13:15:37 -0400

    Added script to make plots for top PCs and Harmony embeddings to see variability of samples and graph-based clusters.

---
+++ code/EDA/Dimensions_plots.R
@@ -0,0 +1,55 @@
+###############################
+# spatial_DG_lifespan project
+# Explore Reduced Dimensions
+# Anthony Ramnauth, Apr 25 2022
+###############################
+
+setwd("/dcs04/lieber/marmaypag/lifespanDG_LIBD001/spatial_DG_lifespan/")
+
+suppressPackageStartupMessages({
+    library(SpatialExperiment)
+    library(here)
+    library(scater)
+    library(scran)
+    library(ggplot2)
+    library(PCAtools)
+    library(ggnewscale)
+    library(spatialLIBD)
+    library(sessioninfo)
+})
+
+# Create directory for QC plots
+dir_plots <- here::here("plots", "Dimensions_plots")
+dir.create(dir_plots, showWarnings = FALSE, recursive = TRUE)
+
+# Load SPE
+spe <- readRDS(here::here("processed-data", "harmony_processed_spe", "harmony_spe.rds"))
+
+# Percentage of variance explained is in the attributes.
+percent.var <- attr(reducedDim(spe), "percentVar")
+
+chosen.elbow <- findElbowPoint(percent.var)
+chosen.elbow
+
+# Elbow plot of PCs & plot Reduced Dimensions
+pdf(file = here::here("plots", "Dimensions_plots", "Dimensions_plot_spe.pdf"))
+plot(percent.var, xlab="PC", ylab="Variance explained (%)")
+abline(v=chosen.elbow, col="red")
+
+plotReducedDim(spe, dimred = "PCA", ncomponents = 4,
+    colour_by="sample_id")
+plotReducedDim(spe, dimred = "PCA", ncomponents = 4,
+    colour_by="label")
+plotReducedDim(spe, dimred = "HARMONY", ncomponents = 4,
+    colour_by="sample_id")
+plotReducedDim(spe, dimred = "HARMONY", ncomponents = 4,
+    colour_by="label")
+
+dev.off()
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
