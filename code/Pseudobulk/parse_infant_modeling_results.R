8ca87074e65e26f57962d9bf00186fb43804e010
Anthony Ramnauth <aramnau1@jh.edu>
2022-05-19 16:15:05 -0400

    Updated infant only DE anaylysis script & plot.

--- code/Pseudobulk/parse_infant_modeling_results.R
+++ code/Pseudobulk/parse_infant_modeling_results.R
@@ -35,14 +35,6 @@ t0_contrasts <- sapply(eb0_list, function(x) {
 })
 rownames(t0_contrasts) <- rownames(eb_contrasts)
 summary(fdrs0_contrasts < 0.05)
-#    1               2               3               4               5               6               7
-# Mode :logical   Mode :logical   Mode :logical   Mode :logical   Mode :logical   Mode :logical   Mode :logical
-# FALSE:11247     FALSE:8221      FALSE:8058      FALSE:11280     FALSE:8573      FALSE:10573     FALSE:11311
-# TRUE :65        TRUE :3091      TRUE :3254      TRUE :32        TRUE :2739      TRUE :739       TRUE :1
-#     8
-# Mode :logical
-# FALSE:5263
-# TRUE :6049
 
 # Merge statistics
 f_merge <- function(p, fdr, t) {
@@ -66,7 +58,6 @@ head(results_specificity)
 pvals_contrasts <- eb_contrasts$p.value
 fdrs_contrasts <- apply(pvals_contrasts, 2, p.adjust, "fdr")
 dim(pvals_contrasts)
-# [1] 11312    21
 summary(fdrs_contrasts < 0.05)
 
 results_pairwise <-
@@ -97,13 +88,6 @@ results_anova <-
     f_rename(f_stats, "f", "f_stat"), "p_value"
   ), "fdr"), "Amean")
 head(results_anova)
-#  f_stat_full p_value_full     fdr_full full_AveExpr         ensembl      gene
-#1    49.88784 5.310201e-24 7.025613e-24     4.014965 ENSG00000228794 LINC01128
-#2   404.01528 8.619080e-49 3.263020e-48     5.279721 ENSG00000188976     NOC2L
-#3   176.07873 1.212845e-38 2.763285e-38     5.734500 ENSG00000188290      HES4
-#4   124.11656 1.863245e-34 3.663658e-34     5.552926 ENSG00000187608     ISG15
-#5    20.45159 6.195671e-15 6.485188e-15     2.972562 ENSG00000188157      AGRN
-#6    60.09126 4.597691e-26 6.539555e-26     3.580353 ENSG00000131591  C1orf159
 
 modeling_results <- list(
   "anova" = results_anova,
@@ -114,25 +98,26 @@ modeling_results <- list(
 # Save modeling results
 saveRDS(modeling_results, file = here::here("processed-data", "pseudobulk_spe", "infant_modeling_results.rds"))
 
-length(which(modeling_results$enrichment$fdr_1 < 0.05))
-
-length(which(modeling_results$enrichment$fdr_2 < 0.05))
-
-length(which(modeling_results$enrichment$fdr_3 < 0.05))
-
-length(which(modeling_results$enrichment$fdr_4 < 0.05))
-
-length(which(modeling_results$enrichment$fdr_5 < 0.05))
-
-length(which(modeling_results$enrichment$fdr_6 < 0.05))
-
-length(which(modeling_results$enrichment$fdr_7 < 0.05))
-
-length(which(modeling_results$enrichment$fdr_8 < 0.05))
-
+length(which(infant_modeling_results$enrichment$fdr_1 < 0.05))
+# 0
+length(which(infant_modeling_results$enrichment$fdr_2 < 0.05))
+# 0
+length(which(infant_modeling_results$enrichment$fdr_3 < 0.05))
+# 3
+length(which(infant_modeling_results$enrichment$fdr_4 < 0.05))
+# 0
+length(which(infant_modeling_results$enrichment$fdr_5 < 0.05))
+# 0
+length(which(infant_modeling_results$enrichment$fdr_6 < 0.05))
+# 0
+length(which(infant_modeling_results$enrichment$fdr_7 < 0.05))
+# 0
+length(which(infant_modeling_results$enrichment$fdr_8 < 0.05))
+# 2239
+## Weird that so many are zero!
 
 cluster <- c(1:8)
-genes <- c(65,3091,3254,32,2739,739,1,6049)
+genes <- c(0,0,3,0,0,0,0,2239)
 df <- data.frame(cluster, genes)
 pdf(file = here::here("plots","pseudobulked","plot_infant_enrichment_DEGs.pdf"))
 plot(df$genes~df$cluster)
