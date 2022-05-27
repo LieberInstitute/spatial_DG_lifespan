4427add2f40ca4e242a62413bdefccc5497021ac
Anthony Ramnauth <aramnau1@jh.edu>
2022-05-27 18:26:29 -0400

    Added R scripts to do DE analysis and GO on enriched genes for separate age bins.

---
+++ code/Pseudobulk/DE_pseudobulk_teen.R
@@ -0,0 +1,161 @@
+###################################################
+# spatial_DG_lifespan project
+# DE analysis of pseudo-bulked BayesSpace clusters
+# Anthony Ramnauth, May 27 2022
+###################################################
+
+suppressPackageStartupMessages({
+    library(SpatialExperiment)
+    library(here)
+    library(sessioninfo)
+    library(SingleCellExperiment)
+    library(rafalib)
+    library(limma)
+})
+
+# Load SPE
+spe_pseudo <- readRDS(here::here("processed-data", "pseudobulk_spe", "pseudobulk_spe.rds"))
+
+## subset spe data based on age bin
+teen_spe_pseudo <- spe_pseudo[, spe_pseudo$age_bin %in% c("Teen")]
+
+# Format spe object for DE models
+colData(teen_spe_pseudo) <- colData(teen_spe_pseudo)[, sort(c(
+    "age",
+    "sample_id",
+    "BayesSpace",
+    "sex",
+    "race",
+    "ncells"
+))]
+
+colData(teen_spe_pseudo)
+
+colData(teen_spe_pseudo)$ncells <- as.numeric(colData(teen_spe_pseudo)$ncells)
+colData(teen_spe_pseudo)$race <- as.factor(colData(teen_spe_pseudo)$race)
+colData(teen_spe_pseudo)$sample_id <- as.factor(colData(teen_spe_pseudo)$sample_id)
+colData(teen_spe_pseudo)$sex <- as.factor(colData(teen_spe_pseudo)$sex)
+
+colData(teen_spe_pseudo)
+
+# Drop things we don't need
+spatialCoords(teen_spe_pseudo) <- NULL
+imgData(teen_spe_pseudo) <- NULL
+
+# Extract the data
+mat <- assays(teen_spe_pseudo)$logcounts
+
+# Matrix for a regression-like model
+mod <- with(colData(teen_spe_pseudo), model.matrix(~0 + BayesSpace))
+
+# Compute correlation
+corfit <- duplicateCorrelation(mat, mod, block = teen_spe_pseudo$sample_id)
+
+######### ENRICHMENT t-stats ######################
+
+cluster_id <- splitit(teen_spe_pseudo$BayesSpace)
+
+eb0_list <- lapply(cluster_id, function(x) {
+  res <- rep(0, ncol(teen_spe_pseudo))
+  res[x] <- 1
+  m <- model.matrix(~res)
+  eBayes(
+    lmFit(
+      mat,
+      design = m,
+      block = teen_spe_pseudo$sample_id,
+      correlation = corfit$consensus.correlation
+    )
+  )
+})
+
+######### PAIRWISE t-stats ######################
+
+fit <-
+  lmFit(
+    mat,
+    design = mod,
+    block = teen_spe_pseudo$sample_id,
+    correlation = corfit$consensus.correlation
+  )
+eb <- eBayes(fit)
+
+## Define the contrasts for each group vs another one
+cluster_combs <- combn(colnames(mod), 2)
+cluster_constrats <- apply(cluster_combs, 2, function(x) {
+  z <- paste(x, collapse = "-")
+  makeContrasts(contrasts = z, levels = mod)
+})
+rownames(cluster_constrats) <- colnames(mod)
+colnames(cluster_constrats) <-
+  apply(cluster_combs, 2, paste, collapse = "-")
+eb_contrasts <- eBayes(contrasts.fit(fit, cluster_constrats))
+
+######### ANOVA t-stats ######################
+
+## From layer_specificity.R
+fit_f_model <- function(spe) {
+
+  ## Extract the data
+  mat <- assays(spe)$logcounts
+
+
+  ## Build a group model
+  mod <- with(colData(spe), model.matrix(~BayesSpace))
+  colnames(mod) <- gsub("clusters", "", colnames(mod))
+
+  #already made in beginning of script #remember to adjust for age or sex
+  corfit <-
+    duplicateCorrelation(mat, mod, block = spe$sample_id)
+  message(paste(Sys.time(), "correlation:", corfit$consensus.correlation))
+  fit <-
+    lmFit(
+      mat,
+      design = mod,
+      block = spe$sample_id,
+      correlation = corfit$consensus.correlation
+    )
+  eb <- eBayes(fit)
+  return(eb)
+}
+
+ebF_list <-
+  lapply(list("full" = teen_spe_pseudo), fit_f_model)
+
+## Extract F-statistics
+f_stats <- do.call(cbind, lapply(names(ebF_list), function(i) {
+    x <- ebF_list[[i]]
+    top <-
+        topTable(
+            x,
+            coef = 2:8,
+            sort.by = "none",
+            number = length(x$F)
+        )
+    # identical(p.adjust(top$P.Value, 'fdr'), top$adj.P.Val)
+    res <- data.frame(
+        "f" = top$F,
+        "p_value" = top$P.Value,
+        "fdr" = top$adj.P.Val,
+        "AveExpr" = top$AveExpr,
+        stringsAsFactors = FALSE
+    )
+    colnames(res) <- paste0(i, "_", colnames(res))
+    return(res)
+}))
+f_stats$ensembl <- rownames(teen_spe_pseudo)
+f_stats$gene <- rowData(teen_spe_pseudo)$gene_name
+rownames(f_stats) <- NULL
+
+head(f_stats)
+
+save(f_stats, eb0_list, eb_contrasts,
+    file = here::here("processed-data", "pseudobulk_spe", "teen_bayesspace_cluster_modeling_results.Rdata"))
+
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
