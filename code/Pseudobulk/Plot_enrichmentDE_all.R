4840e29707ebee1bbe3a7581104d02ae5c4a4e78
Anthony Ramnauth <aramnau1@jh.edu>
2022-07-18 18:03:15 -0400

    Updating enrichment results from pseudobulk for .csv lists of enriched genes for each BayesSpace cluster.

--- code/Pseudobulk/Plot_enrichmentDE_all.R
+++ code/Pseudobulk/Plot_enrichmentDE_all.R
@@ -23,6 +23,183 @@ spe_pseudo <- readRDS(here::here("processed-data", "pseudobulk_spe", "pseudobulk
 # Load modeling results
 modeling_results <- readRDS(file = here::here("processed-data","pseudobulk_spe","modeling_results.rds"))
 
+################################################################
+# Make a .csv list of enriched genes for each BayesSpace Cluster
+################################################################
+
+# Make a data frame summary
+clust_1_enriched_summary <- data.frame(
+    gene_id = modeling_results$enrichment$ensembl,
+    gene_name = modeling_results$enrichment$gene,
+    t_stat = modeling_results$enrichment$t_stat_1,
+    p_val = modeling_results$enrichment$p_value_1,
+    FDR = modeling_results$enrichment$fdr_1
+)
+
+clust_1_enriched_summary <- clust_1_enriched_summary %>%
+    filter(FDR < 0.05) %>%
+    filter(p_val < 0.05)
+
+clust_1_enriched_summary <- arrange(clust_1_enriched_summary, desc(t_stat))
+
+# directory to save lists
+dir_outputs <- here("processed-data", "BayesSpace")
+fn_out_1 <- file.path(dir_outputs, "Clust_1_enriched_results")
+
+# Export summary as .csv file
+write.csv(clust_1_enriched_summary,fn_out_1, row.names = FALSE)
+
+# Make a data frame summary
+clust_2_enriched_summary <- data.frame(
+    gene_id = modeling_results$enrichment$ensembl,
+    gene_name = modeling_results$enrichment$gene,
+    t_stat = modeling_results$enrichment$t_stat_2,
+    p_val = modeling_results$enrichment$p_value_2,
+    FDR = modeling_results$enrichment$fdr_2
+)
+
+clust_2_enriched_summary <- clust_2_enriched_summary %>%
+    filter(FDR < 0.05) %>%
+    filter(p_val < 0.05)
+
+clust_2_enriched_summary <- arrange(clust_2_enriched_summary, desc(t_stat))
+
+# directory to save lists
+fn_out_2 <- file.path(dir_outputs, "Clust_2_enriched_results")
+
+# Export summary as .csv file
+write.csv(clust_2_enriched_summary,fn_out_2, row.names = FALSE)
+
+# Make a data frame summary
+GCL_enriched_summary <- data.frame(
+    gene_id = modeling_results$enrichment$ensembl,
+    gene_name = modeling_results$enrichment$gene,
+    t_stat = modeling_results$enrichment$t_stat_3,
+    p_val = modeling_results$enrichment$p_value_3,
+    FDR = modeling_results$enrichment$fdr_3
+)
+
+GCL_enriched_summary <- GCL_enriched_summary %>%
+    filter(FDR < 0.05) %>%
+    filter(p_val < 0.05)
+
+GCL_enriched_summary <- arrange(GCL_enriched_summary, desc(t_stat))
+
+# directory to save lists
+fn_out_GCL<- file.path(dir_outputs, "GCL_enriched_results")
+
+# Export summary as .csv file
+write.csv(GCL_enriched_summary,fn_out_GCL, row.names = FALSE)
+
+# Make a data frame summary
+SGZ_enriched_summary <- data.frame(
+    gene_id = modeling_results$enrichment$ensembl,
+    gene_name = modeling_results$enrichment$gene,
+    t_stat = modeling_results$enrichment$t_stat_4,
+    p_val = modeling_results$enrichment$p_value_4,
+    FDR = modeling_results$enrichment$fdr_4
+)
+
+SGZ_enriched_summary <- SGZ_enriched_summary %>%
+    filter(FDR < 0.05) %>%
+    filter(p_val < 0.05)
+
+SGZ_enriched_summary <- arrange(SGZ_enriched_summary, desc(t_stat))
+
+# directory to save lists
+fn_out_SGZ<- file.path(dir_outputs, "SGZ_enriched_results")
+
+# Export summary as .csv file
+write.csv(SGZ_enriched_summary,fn_out_SGZ, row.names = FALSE)
+
+# Make a data frame summary
+CA4_enriched_summary <- data.frame(
+    gene_id = modeling_results$enrichment$ensembl,
+    gene_name = modeling_results$enrichment$gene,
+    t_stat = modeling_results$enrichment$t_stat_5,
+    p_val = modeling_results$enrichment$p_value_5,
+    FDR = modeling_results$enrichment$fdr_5
+)
+
+CA4_enriched_summary <- CA4_enriched_summary %>%
+    filter(FDR < 0.05) %>%
+    filter(p_val < 0.05)
+
+CA4_enriched_summary <- arrange(CA4_enriched_summary, desc(t_stat))
+
+# directory to save lists
+fn_out_CA4<- file.path(dir_outputs, "CA4_enriched_results")
+
+# Export summary as .csv file
+write.csv(CA4_enriched_summary,fn_out_CA4, row.names = FALSE)
+
+# Make a data frame summary
+CA3_enriched_summary <- data.frame(
+    gene_id = modeling_results$enrichment$ensembl,
+    gene_name = modeling_results$enrichment$gene,
+    t_stat = modeling_results$enrichment$t_stat_6,
+    p_val = modeling_results$enrichment$p_value_6,
+    FDR = modeling_results$enrichment$fdr_6
+)
+
+CA3_enriched_summary <- CA3_enriched_summary %>%
+    filter(FDR < 0.05) %>%
+    filter(p_val < 0.05)
+
+CA3_enriched_summary <- arrange(CA3_enriched_summary, desc(t_stat))
+
+# directory to save lists
+fn_out_CA3<- file.path(dir_outputs, "CA3_enriched_results")
+
+# Export summary as .csv file
+write.csv(CA3_enriched_summary,fn_out_CA3, row.names = FALSE)
+
+# Make a data frame summary
+ML_enriched_summary <- data.frame(
+    gene_id = modeling_results$enrichment$ensembl,
+    gene_name = modeling_results$enrichment$gene,
+    t_stat = modeling_results$enrichment$t_stat_7,
+    p_val = modeling_results$enrichment$p_value_7,
+    FDR = modeling_results$enrichment$fdr_7
+)
+
+ML_enriched_summary <- ML_enriched_summary %>%
+    filter(FDR < 0.05) %>%
+    filter(p_val < 0.05)
+
+ML_enriched_summary <- arrange(ML_enriched_summary, desc(t_stat))
+
+# directory to save lists
+fn_out_ML<- file.path(dir_outputs, "ML_enriched_results")
+
+# Export summary as .csv file
+write.csv(ML_enriched_summary,fn_out_ML, row.names = FALSE)
+
+# Make a data frame summary
+clust_8_enriched_summary <- data.frame(
+    gene_id = modeling_results$enrichment$ensembl,
+    gene_name = modeling_results$enrichment$gene,
+    t_stat = modeling_results$enrichment$t_stat_8,
+    p_val = modeling_results$enrichment$p_value_8,
+    FDR = modeling_results$enrichment$fdr_8
+)
+
+clust_8_enriched_summary <- clust_8_enriched_summary %>%
+    filter(FDR < 0.05) %>%
+    filter(p_val < 0.05)
+
+clust_8_enriched_summary <- arrange(clust_8_enriched_summary, desc(t_stat))
+
+# directory to save lists
+fn_out_8<- file.path(dir_outputs, "Clust_8_enriched_results")
+
+# Export summary as .csv file
+write.csv(clust_8_enriched_summary,fn_out_8, row.names = FALSE)
+
+################################################
+# Plot top enriched genes per BayesSpace Cluster
+################################################
+
 # Get mean expression
 mat <- assays(spe_pseudo)$logcounts
 
