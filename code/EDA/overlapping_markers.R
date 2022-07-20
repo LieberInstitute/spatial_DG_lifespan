e7af62fdcea409a8e3f6d91ae23dedb383d36736
Anthony Ramnauth <aramnau1@jh.edu>
2022-07-19 22:35:13 -0400

    Updated script for binomial, enriched, & SVG overlapping gene markers per BayesSpace clusters. Ordered by enrichment.

--- code/EDA/overlapping_markers.R
+++ code/EDA/overlapping_markers.R
@@ -6,6 +6,7 @@
 
 library(dplyr)
 library(sessioninfo)
+library(here)
 
 SVG <- read.csv(here::here("processed-data", "nnSVG", "BayesSpace", "DG_BayesSpace_nnSVG_avgrank.csv"))
 binom1 <- read.csv(here::here("processed-data", "BayesSpace", "Clust_1_binomial_test_results.csv"))
@@ -31,8 +32,8 @@ bi1 <- c(binom1$gene_name)
 svg_list <- c(SVG$gene_name)
 
 # Find gene names from one vector in the other
-bien1 <- en1[bi1 %in% en1]
-svg1 <- svg_list[bien1 %in% svg_list]
+bien1 <- en1[en1 %in% bi1]
+svg1 <- bien1[bien1 %in% svg_list]
 svg1 <- svg1[!is.na(svg1)]
 
 # Create vectors of the gene names
@@ -40,8 +41,8 @@ en2 <- c(enriched2$gene_name)
 bi2 <- c(binom2$gene_name)
 
 # Find gene names from one vector in the other
-bien2 <- en2[bi2 %in% en2]
-svg2 <- svg_list[bien2 %in% svg_list]
+bien2 <- en2[en2 %in% bi2]
+svg2 <- bien2[bien2 %in% svg_list]
 svg2 <- svg2[!is.na(svg2)]
 
 # Create vectors of the gene names
@@ -49,8 +50,8 @@ enGCL <- c(enrichedGCL$gene_name)
 biGCL <- c(binomGCL$gene_name)
 
 # Find gene names from one vector in the other
-bienGCL <- enGCL[biGCL %in% enGCL]
-svgGCL <- svg_list[bienGCL %in% svg_list]
+bienGCL <- enGCL[enGCL %in% biGCL]
+svgGCL <- bienGCL[bienGCL %in% svg_list]
 svgGCL <- svgGCL[!is.na(svgGCL)]
 
 # Create vectors of the gene names
@@ -58,8 +59,8 @@ enSGZ <- c(enrichedSGZ$gene_name)
 biSGZ <- c(binomSGZ$gene_name)
 
 # Find gene names from one vector in the other
-bienSGZ <- enSGZ[biSGZ %in% enSGZ]
-svgSGZ <- svg_list[bienSGZ %in% svg_list]
+bienSGZ <- enSGZ[enSGZ %in% biSGZ]
+svgSGZ <- bienSGZ[bienSGZ %in% svg_list]
 svgSGZ <- svgSGZ[!is.na(svgSGZ)]
 
 # Create vectors of the gene names
@@ -67,8 +68,8 @@ enCA4 <- c(enrichedCA4$gene_name)
 biCA4 <- c(binomCA4$gene_name)
 
 # Find gene names from one vector in the other
-bienCA4 <- enCA4[biCA4 %in% enCA4]
-svgCA4 <- svg_list[bienCA4 %in% svg_list]
+bienCA4 <- enCA4[enCA4 %in% biCA4]
+svgCA4 <- bienCA4[bienCA4 %in% svg_list]
 svgCA4 <- svgCA4[!is.na(svgCA4)]
 
 # Create vectors of the gene names
@@ -76,8 +77,8 @@ enCA3 <- c(enrichedCA3$gene_name)
 biCA3 <- c(binomCA3$gene_name)
 
 # Find gene names from one vector in the other
-bienCA3 <- enCA3[biCA3 %in% enCA3]
-svgCA3 <- svg_list[bienCA3 %in% svg_list]
+bienCA3 <- enCA3[enCA3 %in% biCA3]
+svgCA3 <- bienCA3[bienCA3 %in% svg_list]
 svgCA3 <- svgCA3[!is.na(svgCA3)]
 
 # Create vectors of the gene names
@@ -85,8 +86,8 @@ enML <- c(enrichedML$gene_name)
 biML <- c(binomML$gene_name)
 
 # Find gene names from one vector in the other
-bienML <- enML[biML %in% enML]
-svgML <- svg_list[bienML %in% svg_list]
+bienML <- enML[enML %in% biML]
+svgML <- bienML[bienML %in% svg_list]
 svgML <- svgML[!is.na(svgML)]
 
 # Create vectors of the gene names
@@ -94,9 +95,40 @@ en8 <- c(enriched8$gene_name)
 bi8 <- c(binom8$gene_name)
 
 # Find gene names from one vector in the other
-bien8 <- en8[bi8 %in% en8]
-svg8 <- svg_list[bien8 %in% svg_list]
+bien8 <- en8[en8 %in% bi8]
+svg8 <- bien8[bien8 %in% svg_list]
 svg8 <- svg8[!is.na(svg8)]
+
+# Make a dataframe to export to .csv file
+
+max_ln <- max(c(
+    length(svg1),
+    length(svg2),
+    length(svgGCL),
+    length(svgSGZ),
+    length(svgCA4),
+    length(svgCA3),
+    length(svgML),
+    length(svg8)
+    ))
+
+gene_markers <- data.frame(
+    clust_1 = c(svg1, rep(NA, max_ln - length(svg1))),
+    clust_2 = c(svg2, rep(NA, max_ln - length(svg2))),
+    GCL = c(svgGCL, rep(NA, max_ln - length(svgGCL))),
+    SGZ = c(svgSGZ, rep(NA, max_ln - length(svgSGZ))),
+    CA4 = c(svgCA4, rep(NA, max_ln - length(svgCA4))),
+    CA3 = c(svgCA3, rep(NA, max_ln - length(svgCA3))),
+    ML = c(svgML, rep(NA, max_ln - length(svgML))),
+    clust_8 = c(svg8, rep(NA, max_ln - length(svg8)))
+)
+
+# directory to save .csv list
+dir_outputs <- here("processed-data", "nnSVG", "BayesSpace")
+fn_out <- file.path(dir_outputs, "DG_BayesSpace_binomial_enriched_SVG_overlapping_markers")
+
+# Export summary as .csv file
+write.csv(gene_markers,fn_out, row.names = FALSE)
 
 ## Reproducibility information
 print("Reproducibility information:")
