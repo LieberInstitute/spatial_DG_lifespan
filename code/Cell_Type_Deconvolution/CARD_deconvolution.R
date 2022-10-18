a45f6ff6459fed6179919cafac73d9ba001c7ea4
Anthony Ramnauth <aramnau1@jh.edu>
2022-10-18 18:05:35 -0400

    Modified deconvolution script to add dominant cell types to spe object.

--- code/Cell_Type_Deconvolution/CARD_deconvolution.R
+++ code/Cell_Type_Deconvolution/CARD_deconvolution.R
@@ -61,27 +61,27 @@ CARD_obj <- createCARDObject(
 
 CARD_obj <- CARD_deconvolution(CARD_obj)
 
-saveRDS(CARD_deconvo, file = here::here("CARD_obj.rds"))
+saveRDS(CARD_obj, file = here::here("CARD_obj.rds"))
 
-dim(CARD_deconvo@Proportion_CARD)
+dim(CARD_obj@Proportion_CARD)
 dim(spe)
 
-setdiff(rownames(spatial_loc), rownames(CARD_deconvo@Proportion_CARD))
+setdiff(rownames(spatial_loc), rownames(CARD_obj@Proportion_CARD))
 
 # There are 9 Visium spots missing, fill in those blanks!
 
 emptyNaDF <- data.frame(matrix(NA,nrow = 9, ncol = 23))
-rownames(emptyNaDF) <- setdiff(rownames(spatial_loc), rownames(CARD_deconvo@Proportion_CARD))
-colnames(emptyNaDF) <- colnames(CARD_deconvo@Proportion_CARD)
+rownames(emptyNaDF) <- setdiff(rownames(spatial_loc), rownames(CARD_obj@Proportion_CARD))
+colnames(emptyNaDF) <- colnames(CARD_obj@Proportion_CARD)
 
-cell_props <- rbind(CARD_deconvo@Proportion_CARD, emptyNaDF)
+cell_props <- rbind(CARD_obj@Proportion_CARD, emptyNaDF)
 
 stopifnot(rownames(cell_props) == rownames(spatial_loc))
 
 # Reorder dataframe to match the order of spatial barcodes
 
 cell_props <-  cbind(cell_props, rownames(cell_props))
-cell_props <- rename(cell_props, "Barcodes" = "rownames(blah)")
+cell_props <- rename(cell_props, "Barcodes" = "rownames(cell_props)")
 spatial_loc2 <- cbind(spatial_loc, rownames(spatial_loc))
 spatial_loc2 <- rename(spatial_loc2, "Barcodes" = "rownames(spatial_loc)")
 
@@ -92,6 +92,13 @@ stopifnot(rownames(cell_props) == rownames(spatial_loc))
 cell_props[is.na(cell_props)] = 0
 
 colData(spe) <- cbind(colData(spe), cell_props)
+
+# Find dominant celltype for each spot
+p <- cell_props %>%
+ rowwise() %>%
+ mutate(row_max = names(.)[which.max(c_across(everything()))])
+
+colData(spe)$dominant_cell_type <- p$row_max
 
 saveRDS(spe, file = here::here("processed-data", "harmony_processed_spe", "harmony_spe.rds"))
 
