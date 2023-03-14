f20a9bf5f79b3999e22e9b10c09533236862d801
Heena Divecha <heenadivecha@eb-li-612uml85.win.ad.jhu.edu>
2023-03-14 09:40:51 -0400

    Version controlling the app.R script

--- code/03_shinyapp/app.R
+++ code/03_shinyapp/app.R
@@ -9,11 +9,8 @@ options(repos = BiocManager::repositories())
 
 ## Load the data (all paths are relative to this script's location)
 spe <- readRDS("QCed_spe.rds")
-<<<<<<< HEAD
 spe$CellCount <- spe$segmentation_info
-=======
 spe$CellCount <- spe$NBW
->>>>>>> a15aded873b103f13162e665349087c9e927bd3d
 vars <- colnames(colData(spe))
 
 ## Deploy the website
