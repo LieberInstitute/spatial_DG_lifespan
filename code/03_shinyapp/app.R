4031dd6e74135418981aad25b2a36ab1ef8a6d73
rmill162 <rmill162@eb-li-qpmy24h3.win.ad.jhu.edu>
2023-09-18 12:42:52 -0400

    appName change in deploy.R

--- code/03_shinyapp/app.R
+++ code/03_shinyapp/app.R
@@ -11,6 +11,7 @@ options(repos = BiocManager::repositories())
 spe <- readRDS("spe.rds")
 spe_pseudo <- readRDS("pseudobulk_spe.rds")
 modeling_results <- readRDS("modeling_results.rds")
+sig_genes <- readRDS("sig_genes_subset.rds")
 vars <- colnames(colData(spe))
 
 ## Deploy the website
@@ -18,7 +19,7 @@ spatialLIBD::run_app(
     spe,
     sce_layer = spe_pseudo,
     modeling_results = modeling_results,
-    sig_genes = NULL,
+    sig_genes = sig_genes,
     title = "spatial_DG_lifespan, Visium",
     spe_discrete_vars = c("BayesSpace", "ManualAnnotation"),
     spe_continuous_vars = c("sum_umi", "sum_gene", "expr_chrM", "expr_chrM_ratio"),
